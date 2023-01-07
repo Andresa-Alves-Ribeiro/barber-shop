d4f248d2004a8f9c2b93b5b3433f1156916f11b7
Andresa Alves <andresa_15ga@hotmail.com>
2023-01-07 19:15:29 -0300

    edição do header

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src='./assets/barbearia-logo.png' alt='Logo barbearia'></img>
         <p>Barber Shop</p>
       </header>
 
